fac56154ac94900689d38bff95d7af14f8a87906
CryerJonah <160608988+CryerJonah@users.noreply.github.com>
2024-02-20 18:01:58 -0500

    Add files via upload

---
+++ website.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title= "jonah", page_icon=":tada:", layout= "wide")
+
+st.subheader("hi, im Jonah Cryer. ")
+st.title("I am a beginning coder trying to learn python.")
+st.write("I think programming is a useful skill, and everyone should learn. I want to learn to program, so that is why i am making this website about me")
